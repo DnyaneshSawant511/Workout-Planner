b189b8b993dd272fd525fbf25f3b1331b1ada6ce
Dnyanesh Sawant <sawantdnyanesh511@gmail.com>
2024-04-02 00:48:47 +0530

    global middleware

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,18 @@
 const express = require('express');
+require('dotenv').config();
 
 const app = express();
+
+//middleware
+app.use((req, res, next) => {
+    console.log(req.path, req.method);
+    next();
+});
 
 app.get('/', (req, res) => {
     res.json({msg : 'welcome!'});
 });
 
-app.listen(4000, () => {
-    console.log('listening on port 4000');
+app.listen(process.env.PORT , () => {
+    console.log(`listening on port ${process.env.PORT}`);
 });
