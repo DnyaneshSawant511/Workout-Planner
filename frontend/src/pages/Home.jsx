df975bbf654c15901d7df2a06fecf1fd0c1a830f
Dnyanesh Sawant <sawantdnyanesh511@gmail.com>
2024-04-06 13:53:08 +0530

    connection done

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,7 +1,27 @@
+import { useEffect, useState } from "react";
+import axios from "axios";
+
 const Home = () => {
+
+    const [workouts, setWorkouts] = useState([]);
+    useEffect(() => {
+        axios.get("http://localhost:4000/api/workouts/")
+        .then((res) => {
+            setWorkouts(res.data.workouts);
+            console.log(workouts);
+        })
+        .catch((error) => {
+            console.log(error);
+        });
+    }, []);
+
     return (
         <div className="home">
-            <h2>hey!</h2>
+            <div className="workouts">
+                {workouts.map((workout) => (
+                    <p key={workout._id}>{workout.title}</p>
+                ))}
+            </div>
         </div>
     );
 }
